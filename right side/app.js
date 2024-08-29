4f28151cb1b9f2c76c6657610bc90f6214c33376
jagreeti-das <162915401+jagreeti-das@users.noreply.github.com>
2024-08-29 21:32:46 +0530

    Add files via upload

---
+++ right side/app.js
@@ -0,0 +1,49 @@
+const burgerImages = document.querySelectorAll('.burger img');  
+const toggleImages = document.querySelectorAll('.burger-toggle img'); 
+let currentIndex =0; 
+ 
+ 
+ toggleImages[currentIndex].classList.add('active');  
+
+ burgerImages.forEach((img ,index) =>{
+ img.addEventListener('click',()=>{ 
+	toggleImages.forEach(toggleImg => toggleImg.classList.remove('active')); 
+	toggleImages[currentIndex].classList.add('active'); 
+	toggleImages[currentIndex].src = burgerImages[index].src; 
+	currentIndex =(currentIndex +1) % toggleImages.length;
+
+ })
+ 
+
+})
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ // Generate random stars
+function createStars(){ 
+	const starsContainers = document.querySelectorById('stars');  
+	const windowWidth = window.innerWidth; 
+	const windowHeight = window.innerHeight;
+
+for(let i =0;i<100;i++){ 
+	const star = document.createElement('div');
+	star.className ='star'; 
+	star.style.left = '${Math.random() * windowWidth}px'; 
+	star.style.top = '${Math.random() * windowHeight}px'; 
+	starsContainers.appendChild(star);
+}
+
+} 
+
+// call the createStars function when the page loads 
+
+window.addEventListener('load', createStars);
